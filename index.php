2cc073bac61260d7bf30e4a0832aa702f52774e0
Eduard <eduardkiller@hotmail.com>
2020-06-03 19:21:20 -0300

    Simples calculadora

---
+++ index.php
@@ -0,0 +1,23 @@
+<h1>Calculadora</h1>"
+<table>
+<thead>
+<tr>
+<th>Conta
+</th>
+<th> Resultado
+</th>
+</tr>
+</thead>
+<tbody>
+<?php
+for ($x = 1;$x <=10;$x++){
+	echo "<tr>";
+	for ($y = 1;$y <=10;$y++){
+	echo "<td>$x x $y = </td> <td>" . ($x*$y) . "</td>";
+	}
+	echo "</tr>";
+}
+
+?>
+</tbody>
+</table>
